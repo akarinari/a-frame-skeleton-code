fcfde475fc7cf8b09ad0f39182de5527e29387ab
akari nari <lightakari7@gmail.com>
2023-11-15 21:55:17 +0000

    commit

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+AFRAME.registerComponent ('fish',{
+    schema: {
+        position: {type:'number' },
+        rotation: {type: 'number' },
+        speed: {type: 'number'},
 
+
+    },
+
+    init: function (){
+
+    },
+    tick: function (){
+        
+    }
+
+
+
+
+
+
+})
